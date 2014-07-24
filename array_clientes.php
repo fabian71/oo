d69769aeda5c889f9d3e050388612dcebb53dcb2
Fabiano R <fabiano.r@gmail.com>
2014-07-24 15:35:46 -0700

    adionado mais clinetes no arquivo

--- array_clientes.php
+++ array_clientes.php
@@ -9,3 +9,14 @@ $clientes[] = array('nome' => 'Manoel','telefone' => '442929978', 'cpf' => '7923
 $clientes[] = array('nome' => 'Lucas','telefone' => '446629900', 'cpf' => '0933123457','cidade' => 'Maringá', 'estado' => 'PR','email' => 'lucas@email.com');
 
 $clientes[] = array('nome' => 'Maria','telefone' => '776629922', 'cpf' => '0923123487','cidade' => 'São Paulo', 'estado' => 'SP','email' => 'maria@email.com');
+
+$clientes[] = array('nome' => 'Luiza','telefone' => '119929922', 'cpf' => '7423143457','cidade' => 'São Paulo', 'estado' => 'SP','email' => 'luiza@email.com');
+
+$clientes[] = array('nome' => 'Matias','telefone' => '119923812', 'cpf' => '9275122941','cidade' => 'São Paulo', 'estado' => 'SP','email' => 'matias@email.com');
+
+$clientes[] = array('nome' => 'Marcos','telefone' => '11994423812', 'cpf' => '1275422742','cidade' => 'São Paulo', 'estado' => 'SP','email' => 'marcos@email.com');
+
+$clientes[] = array('nome' => 'Joaquina','telefone' => '11334423812', 'cpf' => '6575338209','cidade' => 'Ribeirão Preto', 'estado' => 'SP','email' => 'joaquina@email.com');
+
+$clientes[] = array('nome' => 'Katia','telefone' => '11332725392', 'cpf' => '99362729214','cidade' => 'São Paulo', 'estado' => 'SP','email' => 'katia@email.com');
+
